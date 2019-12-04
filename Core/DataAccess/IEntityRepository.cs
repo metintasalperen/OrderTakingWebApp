832531e4b38d6d17a74b82a7d0fa54f316b2325e
Bulut KARABIYIK <karabiyik15@itu.edu.tr>
2019-12-04 23:37:45 +0300

    Add the project

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+    }
+}
