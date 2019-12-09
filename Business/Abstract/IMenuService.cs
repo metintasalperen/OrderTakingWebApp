a51a696ce01942b1da5367e65234f6d0d50f48e3
metintasalperen <55763726+metintasalperen@users.noreply.github.com>
2019-12-09 23:58:57 +0300

    Added Service Interfaces

---
+++ Business/Abstract/IMenuService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IMenuService
+    {
+        List<Menu> GetAll();
+    }
+}
---
+++ Business/Abstract/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IOrderService
+    {
+        List<Order> GetAll();
+        /*
+        void Add(Order order);
+        void Update(Order order);
+        void Delete(int orderId);
+        ... all necessary services
+        */
+    }
+}
