5bcedbb4b68d75bc463d32631a708985191a1928
metintasalperen <55763726+metintasalperen@users.noreply.github.com>
2019-12-10 00:49:45 +0300

    Fixed DbContext

--- DataAccess/Concrete/EntityFramework/OrderingContext.cs
+++ DataAccess/Concrete/EntityFramework/OrderingContext.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    class OrderingContext
+    public class OrderingContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Ordering; Trusted_Connection=true");
+        }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Table> Tables { get; set; }
     }
 }
