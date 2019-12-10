7516f231fdb0be81fb7d2c876c0ababfa22c9007
metintasalperen <55763726+metintasalperen@users.noreply.github.com>
2019-12-10 03:41:48 +0300

    Show Menu contents on View

--- WebUI/Controllers/MenuController.cs
+++ WebUI/Controllers/MenuController.cs
@@ -4,15 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Business.Abstract;
 using Microsoft.AspNetCore.Mvc;
+using WebUI.Models;
 
 namespace WebUI.Controllers
 {
     public class MenuController : Controller
     {
         private IMenuService _menuService;
+        public MenuController(IMenuService menuService)
+        {
+            _menuService = menuService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var menu = _menuService.GetAll();
+            MenuViewModel model = new MenuViewModel
+            {
+                Menu = menu
+            };
+            return View(model);
         }
     }
 }
