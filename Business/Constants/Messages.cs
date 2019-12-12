e9a7ff30585f7c5febe69c4a8421f82b1ab0c840
Bulut KARABIYIK <karabiyik15@itu.edu.tr>
2019-12-13 01:37:56 +0300

    Add Messages object.
    Messages object will be used for returning messages.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string LoginSuccess = "Login is successful!";
+        public static string LoginFailed = "Login is failed!";
+        public static string RegisterSuccess = "Register is success!";
+        public static string RegisterFailed = "Register is failed!";
+        public static string AccessTokenCreated = "Access Token is created!";
+    }
+}
