8d857b73da61a8ab033f85041f0c8f7497efa258
Bulut KARABIYIK <karabiyik15@itu.edu.tr>
2019-12-12 22:00:13 +0300

    Add Dtos for login and register.

---
+++ Entities/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+
+namespace Entities.Dtos
+{
+    public class UserForRegisterDto:IDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set;  }
+    }
+}
