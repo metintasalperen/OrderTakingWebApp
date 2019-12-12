8fcd5c0d8c6ed9da7389a04ce7e9988bedad86e6
Bulut KARABIYIK <karabiyik15@itu.edu.tr>
2019-12-13 01:50:01 +0300

    Add unmerged method because of conflict.

--- Business/Concrete/UserManager.cs
+++ Business/Concrete/UserManager.cs
@@ -30,6 +30,11 @@ namespace Business.Concrete
             return _userDal.Get(u => u.LastName == lastName);
         }
 
+        public User GetByUserName(string userName)
+        {
+            return _userDal.Get(u => u.UserName == userName);
+        }
+
         public List<User> GetByRole(string role)
         {
             return _userDal.GetList(p => p.Role == role);
