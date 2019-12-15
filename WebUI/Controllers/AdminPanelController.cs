4290d7eb4de68483463b3d6bbe22be4fb143c988
metintasalperen <55763726+metintasalperen@users.noreply.github.com>
2019-12-15 19:57:23 +0300

    Add controller to admin panel

---
+++ WebUI/Controllers/AdminPanelController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        private IMenuService _menuService;
+
+        public AdminPanelController(IMenuService menuService)
+        {
+            _menuService = menuService;
+        }
+        public IActionResult Index(string category = "none")
+        {
+            var menu = _menuService.GetAll();
+            AdminPanelMenuModel model = new AdminPanelMenuModel
+            {
+                Menu = menu,
+                Categories = _menuService.GetCategories(),
+                CurrentCategory = category
+            };
+            return View(model);
+        }
+    }
+}
