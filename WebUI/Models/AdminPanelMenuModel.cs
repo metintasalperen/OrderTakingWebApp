1ab52947dfe3765961c67b6b7591a6a0ed48dfa5
metintasalperen <55763726+metintasalperen@users.noreply.github.com>
2019-12-15 19:56:04 +0300

    Add menu editor UI to admin panel

---
+++ WebUI/Models/AdminPanelMenuModel.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+{
+    public class AdminPanelMenuModel
+    {
+        public List<MenuItem> Menu { get; set; }
+        public List<string> Categories { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
